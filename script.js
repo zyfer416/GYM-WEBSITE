1b60bd370cbd4f0d869674cd74db795550227630
PriyanshCode10 <iampriyanshprajapati@gmail.com>
2024-10-07 22:43:17 +0530

    Day-5 to Day-7 Completed

---
+++ script.js
@@ -0,0 +1,20 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+const typed = new Typed('.multiple-text',  {
+    strings: ['Physical Fitness', 'Weight Gain', 'Strength Training', 'Fat Lose', 'Weightlifting', 'Running'],
+    typeSpeed: 60,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop: true,
+ });
