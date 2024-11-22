3d638b5e3ca0d8c88d3ab1c4114630b41428f00d
Dev-318368 <aswaldevashish003@gmail.com>
2024-11-22 20:30:34 +0530

    yoga update

---
+++ teacher.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const teacherInfo = document.querySelector(".teacher-info");
+    if (teacherInfo) {
+        teacherInfo.classList.add("fade-in");
+    }
+});
